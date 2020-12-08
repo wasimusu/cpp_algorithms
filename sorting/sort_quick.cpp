31c1f314be5d57d2c092017fdacb5c8b0dae2f91
wasimusu <wasim.khan@aggiemail.usu.edu>
2020-12-07 22:15:24 -0700

    quick sort

---
+++ sorting/sort_quick.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include "../utils.cpp"
+
+
+void generate_random_arrs(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+}
+
+/*
+ * Places everything smaller than the pivot value to the left and greater to the right.
+ * Returns the index of the pivot in the partitioned array
+ * */
+template<class T>
+int partition(T *arr, int left, int right, T pivot) {
+    int leftPointer = left - 1;
+    int rightPointer = right;
+
+    while (true) {
+        while (arr[++leftPointer] < pivot) {}
+        while (rightPointer > 0 && arr[--rightPointer] > pivot) {}
+        if (leftPointer >= rightPointer) break;
+        else {
+            std::swap(arr[leftPointer], arr[rightPointer]);
+        }
+    }
+    std::swap(arr[leftPointer], arr[right]);
+    return leftPointer;
+}
+
+/*
+ * Recursive sorting calls.
+ * Finds the pivot and calls partition() on the left and right partitions
+ * */
+template<class T>
+void quick_sort(T *arr, int left, int right) {
+    if (right - left <= 0) return;
+    T pivotIndex = arr[right]; // The pivotIndex can be of your choice
+    int newPivotIndex = partition(arr, left, right, pivotIndex);
+    quick_sort(arr, left, newPivotIndex - 1);
+    quick_sort(arr, newPivotIndex + 1, right);
+}
+
+template<class T>
+void quicksort(T *arr, int n) {
+    quick_sort(arr, 0, n - 1);
+}
+
+int main() {
+    int n = 1000;
+    int arr[n];
+    for (int _ = 0; _ < 100; _++) {
+        generate_random_arrs(arr, n);
+        quicksort(arr, n);
+    }
+    std::cout << "Successfully sorted using bubble sort! \n";
+    return 0;
+}
